ef681667da3e4afbb5458c07027f124035e4aeb5
selimreza-dev <selimreza.dev@gmail.com>
2025-08-31 07:37:09 +0600

    created dynamic login button

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 document.getElementById('login-btn')
-    .addEventListener('click', function (e) {
-        e.preventDefault();
-        console.log('cliced login btn')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        const savedMobileNumber = '01768873518';
+        const savedPinNumber = '1122';
+
+        // Get user mobile & pin number
+        const userMobileNumber = document.getElementById('mobile-number').value;
+        const userPinNumber = document.getElementById('pin').value;
+
+        if (savedMobileNumber === userMobileNumber && savedPinNumber === userPinNumber) {
+            window.location.href = './pages/home.html';
+            // console.log('i am loggin')
+            document.getElementById('mobile-number').value = '';
+
+            document.getElementById('pin').value = '';
+        } else {
+            document.getElementById('mobile-number').style.borderStyle = 'solid';
+            document.getElementById('mobile-number').style.borderWidth = '1px';
+            document.getElementById('mobile-number').style.borderColor = 'red';
+            document.getElementById('mobile-number').style.outline = 'none';
+
+
+
+            document.getElementById('pin').style.borderStyle = 'solid';
+            document.getElementById('pin').style.borderWidth = '1px';
+            document.getElementById('pin').style.borderColor = 'red';
+            document.getElementById('pin').style.outline = 'none';
+        }
 
     })
